d65d3ab37463a7eca42791cc35489bc92f3bb741
rolsmith <rolanddanielsmith@gmail.com>
2021-05-26 14:48:34 +0100

    Uploading 'Practical Exercises in Swirl' R Script

---
+++ Practical exercises in Swirl.R
@@ -0,0 +1,53 @@
+##%######################################################%##
+#                                                          #
+####              Coursera > R Programming              ####
+####             > Practical R Exercises in             ####
+####      swirl Part 1 #Roland Smith # 26 May 2021      ####
+#                                                          #
+##%######################################################%##
+
+
+install.packages ("swirl")
+library(swirl)
+
+ls()
+rm(list=ls())
+
+swirl()
+Roland
+1
+install_from_swirl("R Programming")
+1
+1
+1
+
+# Section 1: Basic Building Blocks ----
+
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+
+z <- c(1.1,9,3.14)
+?c
+
+?c
+z
+c(z,555,z)
+z*2+100
+my_sqrt <- sqrt(z-1)
+2
+my_sqrt
+my_div <- z/my_sqrt
+2
+my_div
+c(1,2,3,4)+c(0,10)
+c(1,2,3,4)+c(0,10,100)
+
+my_div
+2
+rolanddanielsmith@gmail.com
+
+#Section 2: Workspace and Files ----
+
